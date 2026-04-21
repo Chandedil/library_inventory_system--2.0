b18e7e9fc13bb37c7db65504175b1f6a90ab5d86
Chandedil <christiandedil19@gmail.com>
2026-04-21 15:22:56 +0800

    Add files via upload

---
+++ MalinaoProject.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package My_Classes;
+
+import My_Form.LoginForm;
+
+
+/**
+ *
+ * @author Administrator
+ */
+
+
+public class MalinaoProject {
+    
+    public static void main(String[] args) {
+     
+     LoginForm login = new LoginForm();
+ 
+     login.setVisible(true);
+    }
+    
+}
